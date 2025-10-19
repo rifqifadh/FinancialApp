54b45280516290065a2a379ba56e2dd2a1b2dc3f
Rifqi Fadhlillah <rifqi.fadhlillah@gmail.com>
2025-10-19 13:11:45 +0700

    Initial Commit

---
+++ FinancialApp/FinancialAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinancialAppApp.swift
+//  FinancialApp
+//
+//  Created by Rifqi on 19/10/25.
+//
+
+import SwiftUI
+
+@main
+struct FinancialAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
